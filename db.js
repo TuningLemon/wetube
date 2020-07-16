d0523de627779794a6ba1097cd17daa9d43f8f58
TuningLemon <veststar1@gmail.com>
2020-07-17 07:41:49 +0900

    DB goes to MongoDB ATLAS

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ import dotenv from "dotenv";
 // .env 파일 안에 있는 정보를 불러옴. 찾은 모든 variable을 process.env.key에 저장.
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URL_PROD, {
+  // process.env.MONGO_URL 내 컴퓨터 서버 사용할 때,
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
