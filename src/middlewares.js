3168b5659b3e4a0a79ad2888185e78c382fe4ac6
TuningLemon <veststar1@gmail.com>
2020-07-20 14:07:09 +0900

    setting dotenv

--- src/middlewares.js
+++ src/middlewares.js
@@ -11,8 +11,8 @@ import routes from "./routes";
 dotenv.config();
 
 const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_KEY,
-  secretAccessKey: process.env.AWS_PRIVATE_KEY,
+  accessKeyId: process.env.AMAZON_KEY,
+  secretAccessKey: process.env.AMAZON_PRIVATE_KEY,
   region: "ap-northeast-2",
 });
 
